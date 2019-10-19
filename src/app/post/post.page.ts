7de762008c5ce567364b85f17b9b43a75386eafe
Pratyush Raj <42607563+pratyushraj7@users.noreply.github.com>
2019-10-20 03:42:58 +0530

    nine

--- src/app/post/post.page.ts
+++ src/app/post/post.page.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { UserService } from '../user.service';
+import { firestore } from 'firebase/app'
 
 @Component({
-  selector: 'app-post',
-  templateUrl: './post.page.html',
-  styleUrls: ['./post.page.scss'],
+	selector: 'app-post',
+	templateUrl: './post.page.html',
+	styleUrls: ['./post.page.scss'],
 })
 export class PostPage implements OnInit {
 
-  constructor() { }
+	postID: string
+	post
+	postReference: AngularFirestoreDocument
+	sub
 
-  ngOnInit() {
-  }
+	constructor(
+		private route: ActivatedRoute, 
+		private afs: AngularFirestore,
+		private user: UserService) {
 
-}
+	}
+
+	ngOnInit() {
+		this.postID = this.route.snapshot.paramMap.get('id')
+		this.postReference = this.afs.doc(`posts/${this.postID}`)
+	}
+
+	ngOnDestroy() {
+		this.sub.unsubscribe()
+	}
+
+	
+	}
+
